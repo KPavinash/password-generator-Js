4d525518236e71a6669192bf6a461c3e9fa72c85
Avinash KP <67153670+KPavinash@users.noreply.github.com>
2023-01-17 20:17:57 +0530

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("hi")
+
+function generateButton(){
+    var  generatedPassword = "" ;
+    var string =  "abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var userSlideVal = document.getElementById("myRange").value ;
+    document.getElementById("slide-length").innerHTML= userSlideVal; 
+      for (var i = 0; i < userSlideVal; i++) {
+              var randomNumber = Math.floor(Math.random() * string.length);
+              generatedPassword += string.substring(randomNumber, randomNumber +1);
+       }
+   document.getElementById("result-screen").value= generatedPassword;
+}
+
+function copytext(){
+    var copyText = document.getElementById("result-screen").value;
+    navigator.clipboard.writeText(copyText);
+    alert("Copied the text: " + copyText);
+
+}
+
+   
